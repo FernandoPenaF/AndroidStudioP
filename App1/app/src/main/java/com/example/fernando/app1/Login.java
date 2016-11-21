4dfbee2f8c42037addbbe098fd8be8343ba3a5fb
Fernando <lpeaflo1@itam.mx>
2016-11-21 09:37:03 -0600

    App1 - Funcionalidad de idioma

---
+++ App1/app/src/main/java/com/example/fernando/app1/Login.java
@@ -0,0 +1,59 @@
+package com.example.fernando.app1;
+
+import android.content.Intent;
+import android.support.annotation.StringDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class Login extends AppCompatActivity {
+    private final String[] txtSesion = {"Inicio de sesión", "Login", "Login"};
+    private final String[] txtCorreo = {"Correo electrónico", "E-mail", "Courrier électronique"};
+    private final String[] txtPass = {"Contraseña", "Password", "Mot de passe"};
+    private final String[] txtButton = {"Iniciar sesión", "Login", "Commencer la session"};
+    private final String correo = "expe316@hotmail.com";
+    private final String contraseña = "123";
+    private int selectedLanguage;
+    private TextView label;
+    private EditText mail;
+    private EditText pass;
+    private Button btnAceptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Bundle bundle = this.getIntent().getExtras();
+
+        label = (TextView)findViewById(R.id.lbInicioSesion);
+        mail = (EditText)findViewById(R.id.textMail);
+        pass = (EditText)findViewById(R.id.textPass);
+        btnAceptar = (Button)findViewById(R.id.btnInicia);
+        selectedLanguage = (int) bundle.get("Idioma");
+        setIdioma();
+    }
+
+    private void setIdioma(){
+        label.setText(txtSesion[selectedLanguage]);
+        mail.setHint(txtCorreo[selectedLanguage]);
+        pass.setHint(txtPass[selectedLanguage]);
+        btnAceptar.setText(txtButton[selectedLanguage]);
+    }
+
+    public void iniciarSesion(View v){
+        if(mail.getText().toString().equals(correo) && pass.getText().toString().equals(contraseña)) {
+            Intent intent = new Intent(Login.this, Main2Activity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("Mail", mail.getText().toString());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        } else
+            Toast.makeText(this,"El correo y/o la contraseña NO son correctos", Toast.LENGTH_LONG).show();
+    }
+}
--- App1/app/src/main/java/com/example/fernando/app1/MainActivity.java
+++ App1/app/src/main/java/com/example/fernando/app1/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.fernando.app1;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView label;
+    private RadioButton r1, r2, r3;
+    private Button bt;
+    private int selectedLanguage;
+    private final String[] txtLabels = {"Selecciona el idioma", "Select the language", "Sélectionnez la langue"};
+    private final String[] txtButton = {"Continuar", "Continue", "Continuer"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        label = (TextView)findViewById(R.id.lbIdioma);
+        r1 = (RadioButton)findViewById(R.id.rbEsp);
+        r2 = (RadioButton)findViewById(R.id.rbIng);
+        r3 = (RadioButton)findViewById(R.id.rbFran);
+        bt = (Button)findViewById(R.id.btContinuar);
+        selectedLanguage = 0;
     }
+
+    public void cambiaIdioma(View view){
+        if(r1.isChecked()) {
+            label.setText(txtLabels[0]);
+            bt.setText(txtButton[0]);
+            selectedLanguage = 0;
+        }
+        else if (r2.isChecked()) {
+            label.setText(txtLabels[1]);
+            bt.setText(txtButton[1]);
+            selectedLanguage = 1;
+        }
+        else if (r3.isChecked()) {
+            label.setText(txtLabels[2]);
+            bt.setText(txtButton[2]);
+            selectedLanguage = 2;
+        }
+    }
+
+    public void continuar(View view){
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("Idioma", selectedLanguage);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
 }
