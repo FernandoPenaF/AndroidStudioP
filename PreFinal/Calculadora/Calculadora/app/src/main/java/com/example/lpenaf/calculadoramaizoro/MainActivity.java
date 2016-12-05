0e70f96201641bfad8d572b32056b15605da3b86
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2016-12-05 08:41:28 -0600

    PRE Calculadora - Main Activity implementada y probada

--- PreFinal/Calculadora/Calculadora/app/src/main/java/com/example/lpenaf/calculadoramaizoro/MainActivity.java
+++ PreFinal/Calculadora/Calculadora/app/src/main/java/com/example/lpenaf/calculadoramaizoro/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.lpenaf.calculadoramaizoro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String nombre;
+    private EditText etNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.nombre = "";
+        this.etNombre = (EditText)findViewById(R.id.etNombre);
+    }
+
+    public void iniciarSesion(View view){
+        this.nombre = this.etNombre.getText().toString();
+        if(!nombre.equals("")){
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("nombre", nombre);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }else
+            Toast.makeText(this,"El nombre está vació", Toast.LENGTH_LONG).show();
     }
 }
